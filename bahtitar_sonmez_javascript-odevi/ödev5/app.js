4bceade566ea2791b52b5a51209b0dbb43c97817
bahti41 <116912093+bahti41@users.noreply.github.com>
2023-11-28 23:44:01 +0300

    ödev

--- bahtitar_sonmez_javascript-odevi/ödev5/app.js
+++ bahtitar_sonmez_javascript-odevi/ödev5/app.js
@@ -1,1 +1,63 @@
 "use strict";
+
+// Rastgele bir sayı oluştur
+const targetNumber = Math.floor(Math.random() * 10) + 1;
+
+// Kullanıcının puanını ve kalan hakkını tutan değişkenler
+let score = 0;
+let attempts = 3;
+
+// HTML elementlerine erişim
+const guessInput = document.getElementById("guessInput");
+const resultElement = document.getElementById("result");
+const scoreElement = document.getElementById("scoreValue");
+const attemptsElement = document.getElementById("attemptsValue");
+
+function checkGuess() {
+    // Kullanıcının tahminini al
+    const userGuess = parseInt(guessInput.value);
+
+    // Geçerli bir sayı değilse uyarı ver
+    if (isNaN(userGuess) || userGuess < 1 || userGuess > 10) {
+        resultElement.textContent = "Lütfen 1 ile 10 arasında bir sayı girin.";
+        return;
+    }
+
+    // Tahmin doğruysa
+    if (userGuess === targetNumber) {
+        resultElement.textContent = "Tebrikler! Doğru tahmin!";
+        score += 10;
+        resetGame();
+    } else {
+        // Tahmin yanlışsa
+        resultElement.textContent = "Üzgünüm, yanlış tahmin. Tekrar deneyin.";
+        attempts--;
+
+        // Hakkı kalmadıysa oyunu sıfırla
+        if (attempts === 0) {
+            resultElement.textContent = `Oyun bitti. Doğru sayı ${targetNumber} idi.`;
+            resetGame();
+        }
+    }
+
+    // Puan ve hakkı güncelle
+    scoreElement.textContent = score;
+    attemptsElement.textContent = attempts;
+}
+
+function resetGame() {
+    // Yeni bir sayı seç ve puanı ve hakları sıfırla
+    targetNumber = Math.floor(Math.random() * 10) + 1;
+    score = 0;
+    attempts = 3;
+
+    // Mesajı temizle
+    resultElement.textContent = "";
+
+    // Giriş kutusunu temizle
+    guessInput.value = "";
+
+    // Puan ve hakkı güncelle
+    scoreElement.textContent = score;
+    attemptsElement.textContent = attempts;
+}
--- bahtitar_sonmez_javascript-odevi/ödev6/app.js
+++ bahtitar_sonmez_javascript-odevi/ödev6/app.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let tarayici = 'Edge';
+
+if (tarayici === 'Edge') {
+    alert("Edge browser kullanıyorsun");
+} else if (tarayici === 'Chrome' || tarayici === 'Firefox' || tarayici === 'Safari' || tarayici === 'Opera') {
+    alert('Tamam bunları destekliyoruz.');
+} else {
+    alert('Umarım sayfanız güzel görünüyordur');
+}
